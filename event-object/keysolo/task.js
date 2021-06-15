fa134f2095d6471583309a9b98e558f5dd44b4b2
Alisa A <Jumanji1112@mail.ru>
2021-06-15 19:29:45 +0300

    Задача2

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,15 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
-  }
+    document.addEventListener('keydown', (e) => {
+      if (this.currentSymbol.textContent.toLowerCase() == e.key.toLowerCase()){
+        return this.success();
+      } else {
+        return this.fail();
+      }
+      
+    });
+    }
 
   success() {
     this.currentSymbol.classList.add('symbol_correct');
